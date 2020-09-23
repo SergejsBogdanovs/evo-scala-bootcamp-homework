3c9cafd42e9b64928f1f3c665a237f84d679143a
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2020-09-23 08:29:38 +0300

    Fixed: Implemented Basics lesson homework (lcm, gcd)

--- src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/basics/Basic.scala
+++ src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/basics/Basic.scala
@@ -1,11 +1,13 @@
 package lv.sbogdano.evo.scala.bootcamp.homework.basics
 
+import scala.annotation.tailrec
 import scala.math.abs
 
 object Basic {
 
   def lcm(a: Int, b: Int): Int = abs(a * b) / gcd(a, b)
 
+  @tailrec
   def gcd(a: Int, b: Int): Int = b match {
     case 0 => a
     case _ => gcd(b, a % b)
