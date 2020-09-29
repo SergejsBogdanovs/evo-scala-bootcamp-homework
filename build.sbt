fea62e824bb909be7ecaa3c06e17728f289931e2
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2020-09-29 12:07:07 +0300

    ControlStructures: implement homework using pattern matching in process method

--- build.sbt
+++ build.sbt
@@ -3,3 +3,10 @@ name := "evo-scala-bootcamp-homework"
 version := "0.1"
 
 scalaVersion := "2.13.3"
+
+val catsVersion = "2.2.0"
+
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-core" % catsVersion
+)
+
--- src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/controlstructures/ControlStructures.scala
+++ src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/controlstructures/ControlStructures.scala
@@ -1,10 +1,8 @@
 package lv.sbogdano.evo.scala.bootcamp.homework.controlstructures
 
-import lv.sbogdano.evo.scala.bootcamp.homework.controlstructures.ControlStructures.Command.{Average, Divide, Max, Min, Sum}
+import lv.sbogdano.evo.scala.bootcamp.homework.controlstructures.ControlStructures.Command._
 
-import scala.Left
 import scala.io.Source
-import scala.util.Try
 
 object ControlStructures {
 
@@ -45,45 +43,86 @@ object ControlStructures {
   }
 
   sealed trait Result
-  case class Success(value: String) extends Result
-  case class Fault(value: String) extends Result
+  case class DivideResult(l: List[Double], result: Double) extends Result
+  case class SumResult(l: List[Double], result: Double) extends Result
+  case class AverageResult(l: List[Double], result: Double) extends Result
+  case class MinResult(l: List[Double], result: Double) extends Result
+  case class MaxResult(l: List[Double], result: Double) extends Result
 
-  def parseCommand(x: String): Either[ErrorMessage, Command] = {
+  private def parseCommand(x: String): Either[ErrorMessage, Command] = {
     if (x == null || x.isEmpty) return Left("Error: Command must not be null")
 
     val validCommands = Set("divide", "sum", "average", "min", "max")
     val split = x.split("\\s")
     val command = split.head
-    val numbers = split.tail.flatMap(s => Try(s.toDouble).toOption).toList
+    val numbersStr = split.tail
+
+    if (!numbersStr.map(_.toDoubleOption).forall(_.isDefined)) return Left("Error: Evaluation must contain only numbers")
+
+    val numbers = numbersStr.map(_.toDouble).toList
 
     if (!validCommands.contains(command)) Left("Error: Invalid command") else command match {
       case "divide" => numbers.length match {
         case 2 => Right(Divide(numbers.head, numbers(1)))
         case _ => Left("Error: Divide operation requires 2 numbers")
       }
-      case "sum" => Right(Sum(numbers))
+      case "sum"     => Right(Sum(numbers))
       case "average" => Right(Average(numbers))
-      case "min" => Right(Min(numbers))
-      case "max" => Right(Max(numbers))
+      case "min"     => Right(Min(numbers))
+      case "max"     => Right(Max(numbers))
     }
-
   }
 
   // should return an error (using `Left` channel) in case of division by zero and other
   // invalid operations
-  def calculate(x: Command): Either[ErrorMessage, Result] = {
-    ??? // implement this
+  private def calculate(x: Command): Either[ErrorMessage, Result] = x match {
+    case Divide(_, 0) => Left("Error: division by zero")
+    case Divide(x, y) => Right(DivideResult(x::y::Nil, x / y))
+
+    case Sum(Nil) => Left("Error: no numbers to evaluate sum")
+    case Sum(l)   => Right(SumResult(l, l.sum))
+
+    case Average(Nil) => Left("Error: no numbers to evaluate average")
+    case Average(l)   => Right(AverageResult(l, l.sum / l.length))
+
+    case Min(Nil) => Left("Error: no numbers to evaluate min")
+    case Min(l)   => Right(MinResult(l, l.min))
+
+    case Max(Nil) => Left("Error: no numbers to evaluate max")
+    case Max(l)   => Right(MaxResult(l, l.max))
   }
 
-  def renderResult(x: Result): String = {
-    ??? // implement this
+  private def renderResult(x: Result): String = x match {
+    case DivideResult(l, result)  => formatResult("divide", l, result)
+    case SumResult(l, result)     => formatResult("sum", l, result)
+    case AverageResult(l, result) => formatResult("average", l, result)
+    case MinResult(l, result)     => formatResult("min", l, result)
+    case MaxResult(l, result)     => formatResult("max", l, result)
   }
 
-  def process(x: String): String = {
+  private def formatResult(name: String, numbers: List[Double], result: Double): String = name match {
+    case "divide" =>
+      val retString = "%1.0f divided by %1.0f is %1.1f"
+      retString.format(numbers.head, numbers(1), result)
+    case _ =>
+      val retString = "the %s of %s is %1.1f"
+      val s = numbers.mkString(" ")
+      retString.format(name, s, result)
+  }
+
+  private def process(x: String): String = {
     // this will enable useful operations on Either-s such as `leftMap`
     // (map over the Left channel) and `merge` (convert `Either[A, A]` into `A`),
-    // but you can also avoid using them using pattern matching.
-    //import cats.implicits._
+    // but you can also avoid using them using  pattern matching.
+      //import cats.implicits._
+
+    parseCommand(x) match {
+      case Left(errorMessage) => errorMessage
+      case Right(value) => calculate(value) match {
+        case Left(errorMessage) => errorMessage
+        case Right(value) => renderResult(value)
+      }
+    }
 
     // implement using a for-comprehension
 //    for {
@@ -91,7 +130,6 @@ object ControlStructures {
 //      result <- calculate(command)
 //      rendered <- renderResult(result)
 //    } yield rendered
-    ???
   }
 
   // This `main` method reads lines from stdin, passes each to `process` and outputs the return value to stdout
