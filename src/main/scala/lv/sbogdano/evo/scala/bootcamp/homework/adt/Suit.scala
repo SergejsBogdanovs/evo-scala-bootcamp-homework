e3733f4309a0baa0c3fd238990712d11a9616c42
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2020-10-08 08:27:57 +0300

    Implement Suit adt

--- src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/adt/Suit.scala
+++ src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/adt/Suit.scala
@@ -8,6 +8,14 @@ object Suit {
   case object D extends Suit {val character = "d"}
   case object C extends Suit {val character = "c"}
   case object S extends Suit {val character = "s"}
+
+  def from(value: String): Option[Suit] = value match {
+    case H.character => Some(H)
+    case D.character => Some(D)
+    case C.character => Some(C)
+    case S.character => Some(S)
+    case _ => None
+  }
 }
 
 
