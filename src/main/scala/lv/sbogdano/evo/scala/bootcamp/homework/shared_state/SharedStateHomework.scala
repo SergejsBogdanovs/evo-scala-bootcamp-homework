aa0b2bdcecd3fc3721b1482f35e8bd2701c699fa
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2020-11-14 11:40:55 +0200

    SharedStateHomework.scala: first solution

--- src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/shared_state/SharedStateHomework.scala
+++ src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/shared_state/SharedStateHomework.scala
@@ -2,11 +2,9 @@ package lv.sbogdano.evo.scala.bootcamp.homework.shared_state
 
 import cats.Monad
 import cats.effect.concurrent.Ref
-import cats.effect.{Clock, Concurrent, ExitCode, Fiber, IO, IOApp, Sync, Timer}
+import cats.effect.{Clock, Concurrent, ExitCode, IO, IOApp, Timer}
 import cats.implicits._
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 /*
@@ -16,7 +14,7 @@ import scala.concurrent.duration._
  *
  * Tip: you can use following structure to get current time suspended in effect : Clock[F].realTime(MILLISECONDS).flatMap(...)
  *
- * Cached items should have an expiration timestamp after which they are evicted.
+   * Cached items should have an expiration timestamp after which they are evicted.
  *
  * If we will put a value with the same key then it should renew expiration
  */
@@ -27,11 +25,6 @@ object SharedStateHomework extends IOApp {
 
     def put(key: K, value: V): F[Unit]
   }
-
-  object IosCommon {
-    val logger = Slf4jLogger.getLogger[IO]
-  }
-
 
   class RefCache[F[_] : Clock : Monad, K, V](
                                               state: Ref[F, Map[K, (Long, V)]],
@@ -46,7 +39,8 @@ object SharedStateHomework extends IOApp {
     }
 
     def put(key: K, value: V): F[Unit] = {
-      state.update(f => f ++ Map(key -> (expiresIn.toMillis, value)))
+      val currentTime = System.currentTimeMillis()
+      state.update(f => f ++ Map(key -> (currentTime + expiresIn.toMillis, value)))
     }
 
   }
@@ -57,16 +51,20 @@ object SharedStateHomework extends IOApp {
                                 checkOnExpirationsEvery: FiniteDuration
                               )(implicit T: Timer[F], C: Concurrent[F]): F[Cache[F, K, V]] = {
 
-      val currentTime = T.clock.realTime(MILLISECONDS)
-      val valueTime: F[Long] = currentTime.map(time => time + expiresIn.toMillis)
-
-      val check = C.uncancelable(T.sleep(checkOnExpirationsEvery))
+      def deleteExpiredF(state: Ref[F, Map[K, (Long, V)]]): F[Unit] = {
+        (for {
+          map    <- state.get
+          newMap <- C.delay(map.filter(t => System.currentTimeMillis() - t._2._1 < expiresIn.toMillis))
+          _      <- state.set(newMap)
+          _      <- T.sleep(checkOnExpirationsEvery)
+        } yield ()) >> deleteExpiredF(state)
+      }
 
       for {
-        r <- Ref[F].of(Map[K, (Long, V)]())
+        ref <- Ref.of[F, Map[K, (Long, V)]](Map.empty[K, (Long, V)])
+        _   <- C.start(deleteExpiredF(ref))
       } yield {
-        val cache = new RefCache[F, K, V](r, expiresIn)
-        cache
+        new RefCache[F, K, V](ref, expiresIn)
       }
     }
   }
