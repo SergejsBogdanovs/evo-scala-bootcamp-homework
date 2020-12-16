615b272411ad0db995537d3219df747afc67a689
Sergejs Bogdanovs <>
2020-12-16 20:55:22 +0200

    Add update database with cache when user is disconnected

---
+++ src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/course_project/ws/jobs/JobEntity.scala
@@ -0,0 +1,9 @@
+package lv.sbogdano.evo.scala.bootcamp.homework.course_project.ws.jobs
+
+case class JobEntity(
+                    id: Int,
+                    userLogin: String,
+                    status: String,
+                    priority: String,
+                    station: String
+                    )
