d187d7884ef0426b4acef249a21952d4d610010a
Sergejs Bogdanovs <>
2020-12-19 12:13:20 +0200

    Refactor AuthResponse

--- src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/course_project/domain/auth/AuthResponse.scala
+++ src/main/scala/lv/sbogdano/evo/scala/bootcamp/homework/course_project/domain/auth/AuthResponse.scala
@@ -1,7 +1,8 @@
 package lv.sbogdano.evo.scala.bootcamp.homework.course_project.domain.auth
 
 sealed trait AuthResponse
-case class AuthResponseError(error: String) extends AuthResponse
+sealed trait AuthError extends Throwable with AuthResponse
+case class AuthResponseError(error: String) extends AuthError
 case class AuthResponseSuccess(message: String) extends AuthResponse
 
 
